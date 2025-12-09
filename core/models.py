9003722e8b558916b0e7ead81a8e1a94148f0da1
Юсуф <tuhtaevyusuf@gmail.com>
2025-12-09 16:26:42 +0300

    Обновление settings.py и models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=255)
+    image_url = models.URLField()
+
+    def __str__(self):
+        return self.name
+    
+class Flight(models.Model):
+    date = models.DateField()
+    from_country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='froms')
+    to_country = models.ForeignKey(Country, on_delete=models.CASCADE, related_name='tos')
+
+    def __str__(self):
+        return f"{self.date}: {self.from_country} - {self.to_country}"
